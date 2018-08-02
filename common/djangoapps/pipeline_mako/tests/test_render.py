fd1598b24dc50e18c89ccb508c5901c543a09c59
Michael Youngstrom <myoungstrom@edx.org>
2018-08-02 14:14:05 -0400

    revert skip experiments

--- common/djangoapps/pipeline_mako/tests/test_render.py
+++ common/djangoapps/pipeline_mako/tests/test_render.py
@@ -7,7 +7,6 @@ import os
 from django.conf import settings
 from django.test import TestCase
 from paver.easy import call_task
-import pytest
 
 from pipeline_mako import compressed_css, compressed_js, render_require_js_path_overrides
 
@@ -42,7 +41,6 @@ class RequireJSPathOverridesTest(TestCase):
         self.assertEqual(map(str.strip, result.splitlines()), self.OVERRIDES_JS)
 
 
-@pytest.mark.skipif(settings.ROOT_URLCONF != 'lms.urls', reason="Test only valid for LMS")
 @ddt.ddt
 class PipelineRenderTest(TestCase):
     """Test individual pipeline rendering functions. """
@@ -69,6 +67,7 @@ class PipelineRenderTest(TestCase):
         # Update all static assets.
         call_task('pavelib.assets.update_assets', args=('lms', '--settings=test', '--themes=no'))
 
+    @skipUnless(settings.ROOT_URLCONF == 'lms.urls', 'Test only valid in LMS')
     @ddt.data(
         (True,),
         (False,),
@@ -87,6 +86,7 @@ class PipelineRenderTest(TestCase):
             css_include = compressed_css('style-main-v1', raw=True)
             self.assertIn(u'lms-main-v1.css?raw', css_include)
 
+    @skipUnless(settings.ROOT_URLCONF == 'lms.urls', 'Test only valid in LMS')
     def test_compressed_js(self):
         """
         Verify the behavior of compressed_css, with the pipeline
